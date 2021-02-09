d699568df8c20e3191627bd926ddb5ddabaff79d
EC2 Default User <ec2-user@ip-172-31-10-104.us-east-2.compute.internal>
2021-02-09 17:01:47 +0000

    fixing cors

--- src/main/java/com/in28minutes/fullstack/springboot/maven/crud/springbootcrudfullstackwithmaven/SpringBootFullStackCrudFullStackWithMavenApplication.java
+++ src/main/java/com/in28minutes/fullstack/springboot/maven/crud/springbootcrudfullstackwithmaven/SpringBootFullStackCrudFullStackWithMavenApplication.java
@@ -2,6 +2,9 @@ package com.in28minutes.fullstack.springboot.maven.crud.springbootcrudfullstackw
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin
 
 @SpringBootApplication
 public class SpringBootFullStackCrudFullStackWithMavenApplication {
